afd10f0b0398eb51485c630121cae9f5cb13ef40
Egor Pugin <egor.pugin@gmail.com>
2018-07-30 04:02:48 +0300

    Beautify schema.

--- schema.sql
+++ schema.sql
@@ -18,14 +18,21 @@ PRAGMA foreign_keys = OFF;
 -- Table structure for classes
 -- ----------------------------
 DROP TABLE IF EXISTS "main"."classes";
-CREATE TABLE classes (lesson_id INTEGER PRIMARY KEY, teacher_id INTEGER REFERENCES person (person_id) ON DELETE CASCADE ON UPDATE CASCADE, subject_id INTEGER REFERENCES subject (subject_id) ON DELETE CASCADE ON UPDATE CASCADE);
+CREATE TABLE classes (
+    lesson_id INTEGER PRIMARY KEY,
+    teacher_id INTEGER REFERENCES person (person_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    subject_id INTEGER REFERENCES subject (subject_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 -- ----------------------------
 -- Table structure for groups
 -- ----------------------------
 DROP TABLE IF EXISTS "main"."groups";
-CREATE TABLE groups (group_id INTEGER PRIMARY KEY AUTOINCREMENT, group_name TEXT NOT NULL, type_id INTEGER DEFAULT (1) NOT NULL,
-UNIQUE ("group_name")
+CREATE TABLE groups (
+    group_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    group_name TEXT NOT NULL,
+    type_id INTEGER DEFAULT (1) NOT NULL,
+    UNIQUE ("group_name")
 );
 
 -- ----------------------------
@@ -43,35 +50,46 @@ CREATE TABLE journal (
 -- ----------------------------
 DROP TABLE IF EXISTS "main"."person";
 CREATE TABLE person (
-person_id  INTEGER,
-login  TEXT NOT NULL,
-password  TEXT NOT NULL,
-salt  TEXT NOT NULL,
-cookie  TEXT,
-cookie_expires  TEXT,
-surname  TEXT NOT NULL,
-first_name  TEXT NOT NULL,
-middle_name  TEXT NOT NULL,
-type_id  INTEGER NOT NULL DEFAULT (1),
-PRIMARY KEY ("person_id"),
-UNIQUE ("login"),
-UNIQUE ("cookie")
+    person_id  INTEGER,
+    login  TEXT NOT NULL,
+    password  TEXT NOT NULL,
+    salt  TEXT NOT NULL,
+    cookie  TEXT,
+    cookie_expires  TEXT,
+    surname  TEXT NOT NULL,
+    first_name  TEXT NOT NULL,
+    middle_name  TEXT NOT NULL,
+    type_id  INTEGER NOT NULL DEFAULT (1),
+    PRIMARY KEY ("person_id"),
+    UNIQUE ("login"),
+    UNIQUE ("cookie")
 );
 
 -- ----------------------------
 -- Table structure for person_group
 -- ----------------------------
 DROP TABLE IF EXISTS "main"."person_group";
-CREATE TABLE person_group (person_group_id INTEGER PRIMARY KEY, person_id INTEGER REFERENCES person (person_id) ON DELETE CASCADE ON UPDATE CASCADE, group_id INTEGER REFERENCES "group" (group_id) ON DELETE CASCADE ON UPDATE CASCADE);
+CREATE TABLE person_group (
+    person_group_id INTEGER PRIMARY KEY,
+    person_id INTEGER REFERENCES person (person_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    group_id INTEGER REFERENCES "group" (group_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 
 -- ----------------------------
 -- Table structure for subject
 -- ----------------------------
 DROP TABLE IF EXISTS "main"."subject";
-CREATE TABLE subject (subject_id INTEGER PRIMARY KEY, subject_name TEXT NOT NULL);
+CREATE TABLE subject (
+    subject_id INTEGER PRIMARY KEY,
+    subject_name TEXT NOT NULL
+);
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE course (
-course_id INTEGER PRIMARY KEY, subject_id INTEGER REFERENCES subject (subject_id) NOT NULL, teacher_id INTEGER REFERENCES person (person_id
-) NOT NULL, group_id INTEGER REFERENCES groups (group_id) NOT NULL, semester INTEGER NOT NULL);
+    course_id INTEGER PRIMARY KEY,
+    subject_id INTEGER REFERENCES subject (subject_id) NOT NULL,
+    teacher_id INTEGER REFERENCES person (person_id) NOT NULL,
+    group_id INTEGER REFERENCES groups (group_id) NOT NULL,
+    semester INTEGER NOT NULL
+);
